54efc43824c1f2ea99497da0c7234f511b28eeb8
Serina Rodriguez <178450654+xaniresx@users.noreply.github.com>
2025-12-07 00:09:19 -0500

    Refactor Task constructor and add sample usage
    Updated the Task constructor to use more descriptive property names and adjusted parameter names for clarity.
    
    Added a sample Task instance and console logs for debugging and verification.

--- Final Project/Rodriguez_FinalProject.js
+++ Final Project/Rodriguez_FinalProject.js
@@ -1,13 +1,19 @@
 // Task Constructor
-function Task(taskId, taskName, taskPriority, taskImportance, taskCompletionStatus, taskDate ) {
-    this.id = taskId;
-    this.name = taskName;
-    this.priority = taskPriority;
-    this.isImportant = taskImportance;
-    this.isCompleted = taskCompletionStatus;
-    this.date = taskDate;
+function Task(taskId, taskName, taskPriority, isImportant, isCompleted, taskDate ) {
+    this.taskId = taskId;
+    this.taskName = taskName;
+    this.taskPriority = taskPriority;
+    this.isImportant = isImportant;
+    this.isCompleted = isCompleted;
+    this.taskDate = taskDate;
 };
 
 // Container for div element "taskManager"
 const container = document.getElementById("taskManager");
 
+console.log("JavaScript is connected :)");
+
+// Sample Task
+const task1 = new Task(1, "Complete Final Project", "High", true, false, "12/10/2025");
+
+console.log(task1);
