004a06af6eac30c55987668825d291b9516c6abd
Serina Rodriguez <178450654+xaniresx@users.noreply.github.com>
2025-12-08 00:51:55 -0500

    PASSED ✅ testing for task logging into console!
    Tested console logging for task object -- PASSED! ✅
    
    Deleted console log statements used for testing JS connectivity and a sample Task instance to for code cleanup.

--- Final Project/Rodriguez_FinalProject.js
+++ Final Project/Rodriguez_FinalProject.js
@@ -17,13 +17,6 @@ let taskIDcounter = 0;
 // Container for div element "taskManager"
 const container = document.getElementById("taskManager");
 
-// Test to ensure JavaScript is connected -- REMOVE LATER
-console.log("JavaScript is connected :)");
-
-// Sample Task -- REMOVE LATER
-const task1 = new Task(1, "Complete Final Project", "High", true, false, "12/10/2025");
-console.log(task1);
-
 // Add a title heading to the page
 const heading = document.createElement("h1");
 heading.textContent = "Task Manager";
